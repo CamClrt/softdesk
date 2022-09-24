75bc045a39e9020150d23887ca5b6cceea023be2
CamClrt <camille.clarret@gmail.com>
2022-09-24 21:59:48 +0200

    add serializers in project app

---
+++ projects/serializers.py
@@ -0,0 +1,26 @@
+from projects.models import Project, Issue, Comment, Contributor
+from rest_framework import serializers
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ['id', 'created_time', 'title', 'description', 'type']
+
+
+class IssueSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Issue
+        fields = ['id', 'created_time', 'title', 'description', 'tag', 'priority', 'status']
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'created_time', 'description']
+
+
+class ContributorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contributor
+        fields = ['id', 'created_time', 'permission', 'role']
