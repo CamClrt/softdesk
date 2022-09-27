2dd344bc15f14572993f2d75e51fc93b05fff273
CamClrt <camille.clarret@gmail.com>
2022-09-27 17:20:04 +0200

    refactor projects views + add router in urls

--- projects/urls.py
+++ projects/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from projects import views
 
+router = DefaultRouter()
+router.register(r'projects', views.ProjectViewSet, basename="project")
+
 urlpatterns = [
-    path('projects/', views.ProjectList.as_view()),
-    path('projects/<int:pk>/', views.ProjectDetail.as_view()),
+    path('', include(router.urls)),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,17 +1,18 @@
-from rest_framework import generics, permissions
+from rest_framework import permissions, viewsets
 
 from projects.models import Project
 from projects.serializers import ProjectSerializer
 from users.permissions import IsOwnerOrReadOnly
 
 
-class ProjectList(generics.ListCreateAPIView):
-    queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-
-class ProjectDetail(generics.RetrieveUpdateDestroyAPIView):
+class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    This viewset automatically provides `list`, `create`, `retrieve`,
+    `update` and `destroy` actions.
+    """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
